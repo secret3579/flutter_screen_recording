5b8c76e52b29464f1b92566aa514272dee863033
Huy Nguyen <thaihuy15091992@gmail.com>
2022-06-16 10:48:54 +0700

    Fixed cannot run with sound null safety

--- lib/flutter_screen_recording.dart
+++ lib/flutter_screen_recording.dart
@@ -7,13 +7,13 @@ import 'package:flutter_foreground_plugin/flutter_foreground_plugin.dart';
 class FlutterScreenRecording {
   static const MethodChannel _channel = const MethodChannel('flutter_screen_recording');
 
-  static Future<bool> startRecordScreen(String name, {String titleNotification, String messageNotification}) async {
+  static Future<bool> startRecordScreen(String name, {String? titleNotification, String? messageNotification}) async {
     await _maybeStartFGS(titleNotification, messageNotification);
     final bool start = await _channel.invokeMethod('startRecordScreen', {"name": name, "audio": false});
     return start;
   }
 
-  static Future<bool> startRecordScreenAndAudio(String name, {String titleNotification, String messageNotification}) async {
+  static Future<bool> startRecordScreenAndAudio(String name, {String? titleNotification, String? messageNotification}) async {
     await _maybeStartFGS(titleNotification, messageNotification);
     final bool start = await _channel.invokeMethod('startRecordScreen', {"name": name, "audio": true});
     return start;
@@ -27,7 +27,7 @@ class FlutterScreenRecording {
     return path;
   }
 
-  static  _maybeStartFGS(String titleNotification, String messageNotification) async {
+  static _maybeStartFGS(String? titleNotification, String? messageNotification) async {
     if (Platform.isAndroid) {
       await FlutterForegroundPlugin.setServiceMethodInterval(seconds: 5);
       await FlutterForegroundPlugin.setServiceMethod(globalForegroundService);
@@ -39,8 +39,8 @@ class FlutterScreenRecording {
         onStopped: () {
           print("Foreground on Stopped");
         },
-        title: titleNotification,
-        content: messageNotification,
+        title: titleNotification ?? "",
+        content: messageNotification ?? "",
         iconName: "org_thebus_foregroundserviceplugin_notificationicon",
       );
     }
